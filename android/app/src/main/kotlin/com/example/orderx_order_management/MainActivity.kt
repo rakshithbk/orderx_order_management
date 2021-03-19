cdf7e0cef5679313a30f8ca9d5f42d5e75c775de
RakshithBK <rakkurbk@gmail.com>
2021-03-19 19:11:51 +0530

    Base commit
    - basing folder structuring
    - GitPod files for flutter-web

---
+++ android/app/src/main/kotlin/com/example/orderx_order_management/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.orderx_order_management
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/common/model/menu_item.dart
@@ -0,0 +1,14 @@
+class MenuItem {
+  String id;
+  String name;
+  double price;
+  double offerPrice;
+  String description;
+  DateTime createDate;
+  DateTime updateDate;
+  bool isFeatured;
+  bool isVeg;
+  List<String> category;
+  int totalOrderCount;
+  int daysOrderCount;
+}
